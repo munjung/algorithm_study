e117348bfac8cffebb8cc4b090b3237da7cb01d6
MoonJung <jekang0123@gmail.com>
2018-11-25 13:04:13 +0900

    문자열을 정수로 바꾸기 문제 추가

--- Level1/2016year.py
+++ Level1/2016year.py
@@ -10,3 +10,18 @@ def solution(a, b):
     DAY_STR = ['MON','TUE','WED','THU','FRI','SAT','SUN']
     answer = DAY_STR[datetime.date(2016,a,b).weekday()]
     return answer
+
+
+
+#문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+def solution(n):
+    answer = int(n)
+    return answer
+
+class Solution {
+  public int solution(String s) {
+      int answer = 0;
+      answer = Integer.parseInt(s);
+      return answer;
+  }
+}
