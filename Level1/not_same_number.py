de299f63ad5ccb144a522e0f193d2df89aaa7773
MoonJung <jekang0123@gmail.com>
2018-11-27 13:51:07 +0900

    약수의 합 문제 추가

--- Level1/not_same_number.py
+++ Level1/not_same_number.py
@@ -19,3 +19,12 @@ def solution(arr):
 				answer.append(arr[i])
 	return answer
 
+
+
+# 자연수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+def solution(n):
+    answer = 0
+    for i in range(n, 0, -1):
+        if n % i == 0:
+            answer += i
+    return answer
