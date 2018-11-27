865d296fffbe34c15c25a7e8bdacca698bbe14b4
MoonJung <jekang0123@gmail.com>
2018-11-27 15:01:25 +0900

    평균 구하기 문제 추가 (풀이방법 2개)

--- Level1/division_array.py
+++ Level1/division_array.py
@@ -17,3 +17,16 @@ def solution(arr, divisor):
     	answer.append(-1)
     answer.sort()
     return answer
+
+
+#정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+
+def solution(arr):
+    answer = sum(arr)/len(arr)
+    return answer
+
+def solution(arr):
+    answer = 0
+    for i in range(len(arr)):
+		answer += arr[i]
+	return answer/len(arr)
