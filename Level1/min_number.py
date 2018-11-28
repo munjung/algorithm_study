c20ed327976c4949d4b903f8469d9b0809a4b3fe
MoonJung <jekang0123@gmail.com>
2018-11-28 13:38:54 +0900

    짝수 홀수 문제 추가

--- Level1/min_number.py
+++ Level1/min_number.py
@@ -10,3 +10,13 @@ def solution(arr):
     else:
     	answer.remove(min(answer))
     return answer
+
+
+#정수 num이 짝수일 경우 Even을 반환하고 홀수인 경우 Odd를 반환하는 함수, solution을 완성해주세요.
+
+def solution(num):
+    if num % 2 == 0:
+    	answer = 'Even'
+    else:
+    	answer = 'Odd'
+    return answer
