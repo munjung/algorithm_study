e42a47f73b73e756d9deccc3f6c7f3ddb9b21fd9
MoonJung <jekang0123@gmail.com>
2018-12-14 18:10:56 +0900

    자릿수 더하기 문제 추가

--- Level1/p_y_counts.py
+++ Level1/p_y_counts.py
@@ -28,3 +28,11 @@ def solution(s):
     	answer = True
 
     return answer
+
+# 자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+# 예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+
+def solution(n):
+    list_n = [int(x) for x in str(n)]
+    answer = sum(list_n[0:len(list_n)])
+    return answer
